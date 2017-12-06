c4802b31c30aaa2cb200b51c0111e57bc9e90936
ailihong <33647005+ailihong@users.noreply.github.com>
2017-12-06 12:09:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 '''
 python程序的主程序例程
 '''
+import argparse
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Tensorflow Faster R-CNN demo')
+    parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16 res101]',
+                        choices=NETS.keys(), default='res101')
+    parser.add_argument('--dataset', dest='dataset', help='Trained dataset [pascal_voc pascal_voc_0712]',
+                        choices=DATASETS.keys(), default='pascal_voc_0712')
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    
