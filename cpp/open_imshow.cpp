65363e53216ffb47a598e8c563171fd239b3484f
ailihong <33647005+ailihong@users.noreply.github.com>
2018-03-29 21:36:48 +0800

    Update open_imshow.cpp

--- cpp/open_imshow.cpp
+++ cpp/open_imshow.cpp
@@ -8,3 +8,16 @@ cv::waitKey(20);//一定要加这个，要不然显示不了
 //给mat赋值
 cv::Mat frame( mHeight, mWidth, CV_8UC3 );
 memcpy( frame.data, map.data, map.size );
+
+//open camera
+VideoCapture capture("D:/videos/PetsD2TeC2.avi");  
+//检测是否正常打开:成功打开时，isOpened返回ture  
+if(!capture.isOpened())  
+  cout<<"fail to open!"<<endl;
+//读取下一帧  
+if(!capture.read(frame))  
+{  
+   cout<<"读取视频失败"<<endl;  
+}  
+          
+imshow("frame",frame); 
