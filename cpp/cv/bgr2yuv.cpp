7c0f0505ac443e5c14a2b2980b2496be63f8c087
ailihong <33647005+ailihong@users.noreply.github.com>
2018-04-25 14:09:00 +0800

    Update bgr2yuv.cpp

--- cpp/cv/bgr2yuv.cpp
+++ cpp/cv/bgr2yuv.cpp
@@ -2,3 +2,6 @@ cvtColor(frame, frame_yuv, COLOR_BGR2YUV_I420);
 //imshow("src", frame_yuv);
 //waitKey(1);
 memcpy(buf, frame_yuv.data, 640*480*1.5);
+
+Mat myuv(img->height + img->height / 2, img->width, CV_8UC1, img->img);
+cvtColor(myuv, bgr, CV_YUV2BGR_I420, 0);
