9cb92270ac224d7997dc9487961b2626bb974bc6
ailihong <33647005+ailihong@users.noreply.github.com>
2018-08-28 10:26:44 +0800

    Create capture_from_camera.cpp

---
+++ cpp/cv/capture_from_camera.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <vector>
+#include <fstream>
+extern "C"{
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include <errno.h>
+    #include <dirent.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+}
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std; 
+using namespace cv;
+
+int main(int argc, char** argv) 
+{   
+
+    VideoCapture cap(0);
+    int cnt = 0;
+    string name;
+    char option;
+    while(1)
+    {
+        Mat frame;
+        
+        cap.read(frame);
+        if (frame.empty())
+        {
+            cout << " image is empty" << endl;
+            continue;
+        }
+        
+        imshow("camera", frame);
+        option = waitKey(50);
+        if(option=='s')
+        {
+            cnt++;
+            name = "baijun_"+to_string(cnt)+".jpg";
+            imwrite(name,frame);
+        }
+        else if(option == 'q')
+        {
+            break;
+        }
+    }
+
+    return 0;
+    
+}
