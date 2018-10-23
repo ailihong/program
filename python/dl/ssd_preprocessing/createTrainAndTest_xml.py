d31aa73ce9eb9d0c2ccf97edd206f73f5cafb397
ailihong <33647005+ailihong@users.noreply.github.com>
2018-10-23 16:31:26 +0800

    Create createTrainAndTest_xml.py

---
+++ python/dl/ssd_preprocessing/createTrainAndTest_xml.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+import random
+import argparse
+
+parser = argparse.ArgumentParser(description='batch rename')
+parser.add_argument('--xml', dest='xml', help='file directory',default='None')
+parser.add_argument('--txt', dest='txt', help='file directory',default='None')
+parser.add_argument('--test', dest='test', help='test percent',type=float,default=0.05)
+args = parser.parse_args()
+
+test_percent = args.test
+if args.xml != 'None' or args.txt != 'None':
+    xmlfilepath = args.xml
+    txtsavepath = args.txt
+else:
+    print('enter parameters')
+    exit(0)
+total_xml = os.listdir(xmlfilepath) 
+allNum = len(total_xml)
+test = int(allNum*test_percent)
+
+random_index = random.sample(range(allNum), allNum)
+trainIndex = random_index[:allNum-test]
+testIndex = random_index[allNum-test:]
+
+ftest = open(os.path.join(txtsavepath , 'test.txt'), 'w')  
+ftrain = open(os.path.join(txtsavepath , 'trainval.txt'), 'w')  
+
+for i  in trainIndex:  
+    name=total_xml[i][:-4]+'\n'
+    ftrain.write(name)
+for i  in testIndex:  
+    name=total_xml[i][:-4]+'\n'
+    ftest.write(name)
+      
+ftest.close()
+ftrain.close()
