c622a56ab6be4bcca2d48df470173fe2539636be
ailihong <33647005+ailihong@users.noreply.github.com>
2019-04-22 14:21:02 +0800

    support random pick files

--- python/dl/classify/list_all.py
+++ python/dl/classify/list_all.py
@@ -6,13 +6,17 @@
 
 import os
 import argparse
+import random
 
 def main(args):
     '''main '''
     file_list = os.listdir(args.file_path)
     fout=open(args.list_file,'a')
-    for file_name in file_list:
-        path=os.path.join(args.file_path,file_name)
+    allNum = len(file_list)
+    random_index = random.sample(range(allNum), allNum)
+    saveIndex = random_index[:int(allNum*args.percent)]
+    for i in saveIndex:
+        path=os.path.join(args.file_path,file_list[i])
         fout.write(path+' %d\n'%args.label)
     print('end')
 def parse_args():
@@ -20,6 +24,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path', '-fp',default='None', type=str)
     parser.add_argument('--label','-l', default=-1, type=int)
+    parser.add_argument('--percent','-p', default=1.0, type=float)
     parser.add_argument('--list_file','-lf',type=str, default='None',help='append mode')
     return parser.parse_args()
 
