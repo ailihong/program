037e5792e0a03554c056799ee7eb0f267428f5a4
MichaelBai <33647005+ailihong@users.noreply.github.com>
2019-05-17 16:17:49 +0800

    Update myDataBase.h

--- cpp/mydb/myDataBase.h
+++ cpp/mydb/myDataBase.h
@@ -10,6 +10,20 @@ typedef struct record_t {
     float feature[FEATURE_LENGTH];
 }record;
 
+typedef struct query_simi_t{
+char name[NAME_LENGTH];
+float simility;
+}query_simi;
+
+typedef struct ref_query_simi_t{
+    query_simi * pQuery_simi;
+    int ret_query_num;
+}ref_query_simi;
+
+//返回值(*指针名)(参数列表)
+typedef float (*simility)(float *,float *, int len);
+extern ref_query_simi ret_query;
+
 #define BTYES_NUM_RECOEDS (sizeof(int))
 #define BTYES_A_RECOEDS (sizeof(record))
 
@@ -20,5 +34,9 @@ int addBinFile(record record_a);
 int findBinFile(record * precord);
 int deleteRecordByName(char *searchname);
 int updateRecord(record  record_);
+int closeBinFile();
 
+int query_db(simility ptr_fun, float * feature_query, float thresh, ref_query_simi * pRef_query_result);
+int Top_one(ref_query_simi *pRef_query_simi_);
+void Top_k(ref_query_simi *pRef_query_simi_,int * idx_top, int top_k);
 #endif
