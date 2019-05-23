1a2994d7919f72f663b9bf42bab9f24f87301e02
MichaelBai <33647005+ailihong@users.noreply.github.com>
2019-05-23 12:35:38 +0800

    Update fs_readfile_getline.cpp

--- cpp/fs_readfile_getline.cpp
+++ cpp/fs_readfile_getline.cpp
@@ -9,9 +9,9 @@ int main(int argc, char** argv){
     cout << "enter param" << endl;
     }
     std::ifstream f1(argv[1], std::ios::in);
-    vector<string>words; //创建一个vector<string>对象
-    string      line1; //保存读入的每一行
-    while(getline(f1,line1))//会自动把\n换行符去掉 
+    vector<string>words;
+    string      line1;
+    while(getline(f1,line1))//without \n
     {
         words.push_back(line1);  
     }
