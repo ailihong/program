596d82b5c7b60c81d7c18c9058ff9050c93e1171
MichaelBai <33647005+ailihong@users.noreply.github.com>
2019-07-08 10:40:54 +0800

    Create subModule.py

---
+++ python/logging_test/subModule.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 10:35:44 2019
+
+@author: bai
+"""
+import logging
+
+module_logger = logging.getLogger("mainModule.sub")
+class SubModuleClass(object):
+    def __init__(self):
+        self.logger = logging.getLogger("mainModule.sub.module")
+        self.logger.info("creating an instance in SubModuleClass")
+    def doSomething(self):
+        self.logger.info("do something in SubModule")
+        a = []
+        a.append(1)
+        self.logger.debug("list a = " + str(a))
+        self.logger.info("finish something in SubModuleClass")
+
+def som_function():
+    module_logger.info("call function some_function")
