51c8c6490771198d660ba113884f16712a95438d
MichaelBai <33647005+ailihong@users.noreply.github.com>
2020-02-20 14:55:20 +0800

    Create split_folder_trainval.py

---
+++ pytorch/classification_on_myown_data/split_folder_trainval.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 15:53:41 2020
+
+@author: baijun
+鏍规嵁鍥剧墖鐩綍鎸夋瘮渚嬮殢鏈烘媶鍒嗘垚trainval
+"""
+import argparse
+import os
+import random
+
+parser = argparse.ArgumentParser(description='Train PNet',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-i','--image_path',dest='image_path',help='image path',
+                        default='./',required=True, type=str)
+parser.add_argument('-t','--train_path', dest='train_path', help='training image path',
+                        default='train_data',type=str)
+parser.add_argument('-v','--val_path', dest='val_path', help='val image path',
+                        default='val_data', type=str)
+parser.add_argument('-p','--train_percent', dest='train_percent', help='train image percent,default:0.8',
+                        default=0.8,type=float)
+args = parser.parse_args()
+
+image_path = args.image_path
+train_path = args.train_path
+val_path = args.val_path
+train_percent = args.train_percent
+
+
+if not os.path.exists(val_path):
+    os.system("mkdir -p {}".format(val_path))
+
+class_folder = os.listdir(image_path)
+for folder in class_folder:
+    name_list = os.listdir(os.path.join(image_path,folder))
+    random.shuffle(name_list)
+    train_data_num = int(len(name_list)*train_percent)
+    train_list = name_list[:train_data_num]
+    val_list = name_list[train_data_num:]
+    
+    phase = ['train','val']
+    filelist = {'train':train_list,'val':val_list}
+    savelist = {'train':train_path,'val':val_path}
+    
+    for pha in phase:
+        if not os.path.exists(os.path.join(savelist[pha],folder)):
+            os.system("mkdir -p {}".format(os.path.join(savelist[pha],folder)))
+        
+        for name in filelist[pha]:
+            src_filename = os.path.join(image_path,folder,name)
+            dst_filename = os.path.join(savelist[pha],folder,name)
+            os.system("cp {} {}".format(src_filename,dst_filename))
+            
+print("done")
