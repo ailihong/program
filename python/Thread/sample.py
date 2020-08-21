1c2798ad182f632d53f2b89309462df6a0649e19
MichaelBai <33647005+ailihong@users.noreply.github.com>
2020-08-21 15:57:09 +0800

    Update sample.py

--- python/Thread/sample.py
+++ python/Thread/sample.py
@@ -9,3 +9,19 @@ p = Master(xxx)
 #p.daemon=True#设置为守护进程,主进程结束,子进程强制结束
 p.start()
 p.join()
+
+import threading
+import time
+
+exitFlag = 0
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print ("开始线程：" + self.name)
+        print_time(self.name, self.counter, 5)
+        print ("退出线程：" + self.name)
