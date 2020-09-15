cd0340209a2cb0990d33f7dd8b4ca02229851d09
MichaelBai <33647005+ailihong@users.noreply.github.com>
2020-09-15 13:39:55 +0800

    Create loop_simple.sh
    遍历目录

---
+++ bash/loop_simple.sh
@@ -0,0 +1,8 @@
+for i in `ls temp/*.txt` #temp/1.txt,temp/2.txt
+do
+    #mkdir ./${i%.tar}
+    #tar xvf $i -C ./${i%.tar}
+    echo ${i} #print the whole name,example:temp/1.txt
+    echo ${i%.txt} #exclude the txt,example:temp/1
+    echo debug/`basename ${i}` #out:debug/1.txt
+done
